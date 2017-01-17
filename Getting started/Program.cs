4c2083bed0faa64da95cd5fa4aad9d8ae54fe371
kiick <kiick@abv.bg>
2017-01-17 20:36:50 +0200

    new project

---
+++ Getting started/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getting_started
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Good evening!");
+        }
+    }
+}

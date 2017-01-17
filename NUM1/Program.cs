665c014471d135e6ecac1fdb1ed3b1184b27823c
kiick <kiick@abv.bg>
2017-01-17 20:44:00 +0200

    new projects

---
+++ NUM1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUM1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstLetter = char.Parse(Console.ReadLine());
+            var secondLetter = char.Parse(Console.ReadLine());
+            var excluding = char.Parse(Console.ReadLine());
+            int cnt = 0;
+
+            for (var i = firstLetter; i <= secondLetter; i++)
+            {
+                for (var j = firstLetter; j <= secondLetter; j++)
+                {
+                    for (var k = firstLetter; k <= secondLetter; k++)
+                    {
+                        if (excluding == i 
+                            || excluding == j
+                            || excluding == k)
+                        {
+                            continue;
+                        }
+                        Console.Write($"{i}{j}{k} ");
+                        cnt++;
+                    }
+                }
+            }
+            Console.Write(cnt);
+        }
+    }
+}
